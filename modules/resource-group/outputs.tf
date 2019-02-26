ba2c99d47354ff8a0f85fbac353f285cb262f28a
anoop600 <anoop.jain10@gmail.com>
2019-02-26 12:01:36 +0000

    removed resource group module

--- modules/resource-group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "rs-name" {
-  value = "${azurerm_resource_group.k8s.name}"
-}
-
-output "rs-location" {
-  value = "${azurerm_resource_group.k8s.location}"
-}
--- modules/resource-group/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "resource_group_name" {
-  default = "devops-test"
-}
-
-variable "location" {
-  default = "central us"
-}
